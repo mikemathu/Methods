c8c3198df4d01b425d2515a3ffa7375a982c2f8b
Michael Mathu <mathumichael17@gmail.com>
2022-04-06 13:07:54 +0300

    calling a method

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Methods
+{
+    public class Methods
+    {
+        static void Main(string[] args)
+        {
+            CountToTen();
+        }
+
+        static void CountToTen()
+        {
+            for (int index = 1; index <= 10; index++)
+                Console.WriteLine(index);
+        }
+    }
+}
