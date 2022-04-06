c8f020658e26483b516ac4e3d6539eb192e6c28b
Michael Mathu <mathumichael17@gmail.com>
2022-04-06 14:06:49 +0300

    returning early in the middle of the method

--- Program.cs
+++ Program.cs
@@ -30,7 +30,7 @@
         }*/
 
         //RETURNING STUFF FROM A METHOD
-        static void Main(string[] args)
+        /*static void Main(string[] args)
         {
             int usersNumber = GetNumberFromUser();
 
@@ -51,7 +51,31 @@
             }
 
             return usersNumber;
+        }*/
+
+        //RETURNING 'EARLY' I.E NOT NECESSARILY AT THE END OF THE METHOD
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CalculatePlayerScore());
         }
+
+        static int CalculatePlayerScore()
+        {
+            int livesLeft = 3;
+            int underlingDesroyed = 17;
+            int minionsDestroyed = 4;
+            int bossesDestroyed = 1;
+
+            //If the player is out of lives, they lose all of their points
+            if(livesLeft == 0)
+                return 0;
+
+            //Otherwise, the player get 10 points per underling destroyed, 100 points per minion, and 1000 points per boss
+            return underlingDesroyed*10 + minionsDestroyed*100 + bossesDestroyed*1000;
+        }
+
+
+
 
     }
 }
